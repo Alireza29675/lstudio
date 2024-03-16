8c9117664a0f7236ae51055d3c6670d2687a416d
Alireza <contact@alireza.cc>
2024-03-16 15:41:00 +0100

    refactor: removed web output

--- projects/hightechmess/src/ouputs/web/types.ts
+++
@@ -1,6 +0,0 @@
-import { Clock, Project } from '@lstudio/core'
-import { ClockPayloadType } from '../../clock'
-import { State } from '../../state'
-
-export type ProjectType = Project<ClockPayloadType, State, string>
-export type ClockType = Clock<ClockPayloadType>
