bfcb6a4c06a0d891c2e73abedf9bbfa1693070d8
Alireza <contact@alireza.cc>
2024-03-22 11:57:24 +0100

    feat: added some math utils

--- projects/hightechmess/src/mods/common/math.ts
+++ projects/hightechmess/src/mods/common/math.ts
@@ -1,3 +1,29 @@
+// Easing functions
+export function linear(t: number): number {
+  return t
+}
+
+export function easeIn(t: number): number {
+  return t * t
+}
+
+export function easeOut(t: number): number {
+  return t * (2 - t)
+}
+
+export function easeInOut(t: number): number {
+  return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t
+}
+
+export function spring(t: number): number {
+  return 1 - Math.cos(t * Math.PI * 4) * Math.exp(-t * 6)
+}
+
+export function climax(t: number): number {
+  return 1 - Math.pow(2, -10 * t)
+}
+
+// Random math utilities
 export function randInt(min: number, max: number): number {
   min = Math.ceil(min)
   max = Math.floor(max)
@@ -20,12 +46,13 @@ export function shuffle<T>(arr: T[]): T[] {
   return arr.slice().sort(() => Math.random() - 0.5)
 }
 
+// General Math utilities
 export function clamp(value: number, min: number, max: number): number {
   return Math.min(Math.max(value, min), max)
 }
 
-export function lerp(a: number, b: number, t: number): number {
-  return a + (b - a) * t
+export function lerp(a: number, b: number, t: number, ease: (t: number) => number = linear): number {
+  return a + (b - a) * ease(t)
 }
 
 export function degToRad(deg: number): number {
